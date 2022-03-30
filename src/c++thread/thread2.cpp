e71dcf71a8e5adf0539b9116d66af7959e61ddca
li ning <825255961@qq.com>
2022-03-30 17:44:52 +0800

    添加超时递归锁

--- src/c++thread/thread2.cpp
+++ src/c++thread/thread2.cpp
@@ -118,7 +118,9 @@ struct Calculate1
 };
 
 /*
-
+1.使用递归锁的场景往往都是可以简化的，使用递归互斥锁很容易放纵复杂逻辑的产生，从而导致bug的产生
+2. 递归互斥锁比非递归互斥锁效率低一点
+3. 递归互斥锁虽然允许同一线程获得同一互斥锁的所有权，但是最大次数并未具体说明，一旦超过一定次数，就会抛出std::system错误
 */
 void test2()
 {
@@ -127,6 +129,49 @@ void test2()
   cal.both(6, 3);
 }
 
+/*
+std::timed_mutex超时独占互斥锁,主要是在获取互斥锁资源时增加了超时等待功能，因为不知道获取锁资源需要等待多长时间
+，为了保证不一直等待下去，设置了一个超时时长，超时后线程就可以解除阻塞去做其他的事情了
+多两个成员函数 try_lock_for()和try_lock_until()
+try_lock_for()是当线程获取不到互斥锁资源时，让线程阻塞一定时间
+try_lock_until()是当线程获取不到互斥锁资源时，让线程阻塞到某一个指定的时间点
+关于两个函数的返回值，当得到互斥锁的所有权之后，函数会马上解除阻塞，返回true，如果阻塞的时长用完或达到指定时间点之后
+函数会解除阻塞，返回false
+*/
+timed_mutex g_mutex;
+void work()
+{
+  chrono::seconds timeout(1);
+  while (true)
+  {
+    if (g_mutex.try_lock_for(timeout))
+    {
+      cout << "当前线程id " << this_thread::get_id() << " 得到互斥锁所有权 " << endl;
+      //模拟处理任务用了一定时长
+      this_thread::sleep_for(chrono::seconds(10));
+      g_mutex.unlock();
+      break;
+    }else
+    {
+      cout << "当前线程id " << this_thread::get_id() << " 没有得到互斥锁所有权 " << endl;
+      //模拟其他任务
+      this_thread::sleep_for(chrono::seconds(1));
+    }
+  }
+}
+
+void test3()
+{
+  thread t1(work);
+  thread t2(work);
+  t1.join();
+  t2.join();
+}
+
+/*
+std::recursive_timed_mutex递归超时互斥锁和timed_mutex是一样的，只不过它可以允许一个线程多次获得互斥锁所有权，
+另外也拥有和他一样的弊端，不建议频繁使用
+*/
 
 int main()
 {
@@ -135,5 +180,8 @@ int main()
   test1();
   cout << "-----------------------" << endl;
   test2();
+  cout << "-----------------------" << endl;
+  test3();
+  cout << "-----------------------" << endl;
   return 0;
 }
